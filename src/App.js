c23937dfdc5a57f281740d892795e7af4064c74c
Luz Lugo <luzlugo94@gmail.com>
2022-08-22 21:53:21 -0500

    tarea2
    tarea

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './pet.png';
+
+import hostal from './hostal.jpg';
 import './App.css';
+import Menupeludos from './Menupeludos';
+
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         El lugar donde tus peludos y tu pueden hospedarse en:
-        </p>
-        <a
+      <a
           className="App-link"
           href="https://hostalcasaelprado.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Casa Hostal el prado en Cartagena
-        </a>
-      </header>
+          <img src={hostal} className="App-logoHostal" alt="logo" /> 
+        </a>     
+        
+        <p>
+         El lugar donde tus peludos y tu pueden hospedarse
+        </p>
+       
+        <Menupeludos/>
+      </header >
     </div>
   );
 }
