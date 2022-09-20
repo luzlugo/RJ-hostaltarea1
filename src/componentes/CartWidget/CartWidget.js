1d26daeb2dfb4fa06c99b35475ae46ca0afa12ed
Luz Lugo <luzlugo94@gmail.com>
2022-09-20 00:04:41 -0500

    A
    A

--- src/componentes/CartWidget/CartWidget.js
+++ src/componentes/CartWidget/CartWidget.js
@@ -1,24 +1,23 @@
-
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import './CartWidget.css';
 import shoppingcart from './shoppingcart.png';
+import { CartContext } from '../../context/CartContext';
 
 const Cartwidget = () => {
+
+   const {cartQuantity} = useContext(CartContext)
 
   return(
      
 
 <header className="bg-header">
         <div className='header-container'>
-        
-         <a
-          className="header-title"
-          href="https://hostalcasaelprado.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+         <Link to='/cart'>
+         
           <img src={shoppingcart} className="App-logocart" alt="logo" /> 
-        </a> 
-       
+          </Link>
+           <span>{cartQuantity()}</span>
         </div>
        
       
