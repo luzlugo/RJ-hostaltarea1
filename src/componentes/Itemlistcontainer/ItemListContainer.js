803dc7aa99bc7d66b905828b216168fe060155e5
Luz Lugo <luzlugo94@gmail.com>
2022-09-27 00:54:39 -0500

    b
    b

--- src/componentes/Itemlistcontainer/ItemListContainer.js
+++ src/componentes/Itemlistcontainer/ItemListContainer.js
@@ -1,34 +1,32 @@
 import { useEffect, useState } from "react"
-import { pedirDatos } from "../Helpers/Pedirdatos"
+//import { pedirDatos } from "../Helpers/Pedirdatos"
 import {ItemList} from '../ItemList/ItemList'
 import {useParams} from 'react-router-dom'
+import { collection, getDocs } from "firebase/firestore"
+import {db} from '../../firebase/config'
 
 export const ItemListContainer = () => {
 
     const [productos, setProductos] = useState([])
     const [loading, setLoading] = useState(true)  
     const {categoryId} =useParams()
-    
-   console.log(categoryId)
 
  useEffect(() => {
    setLoading(true)
+     //armar referencia 
+    const productosRef = collection(db, 'productos') 
+    // consumir
+    getDocs(productosRef)
+    .then((resp) => {
+      const productosDB = resp.docs.map((doc)=> ({id: doc.id, ...doc.data()}) )
+      console.log(productosDB)
 
-   pedirDatos()
-    .then((res) => {
-      if (!categoryId){
-        setProductos(res)
-      }else{
-        setProductos(res.filter((prod)=> prod.category === categoryId))
-
-      }    
+      setProductos(productosDB)
     })
-    .catch((error) => {
-        console.log(error)
-    })
-    .finally(()=> {
+    .finally (() => {
       setLoading(false)
     })
+
  }, [categoryId])
  
 
